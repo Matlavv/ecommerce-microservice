fe675cff272808f85c94730aa01978f4e6c55876
remimoul <neptunes92390@outlook.fr>
2024-10-28 16:08:08 +0100

    ✅ Add initial files for user service

--- backend/user-service/src/app.ts
+++ backend/user-service/src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Server is running');
+    res.send('Server is running BONJOUddddegiruytgughdfkugdhfikuR');
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    console.log('coucou');
+    console.log('coucoudfff');
 });
