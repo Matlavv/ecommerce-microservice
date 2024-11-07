c0597463b85616c54c97604d9ba589b18e8bc1a4
remimoul <neptunes92390@outlook.fr>
2024-11-07 09:38:46 +0100

    🚀feat: implement user registration endpoint and update database schema

--- backend/user-service/src/app.ts
+++ backend/user-service/src/app.ts
@@ -1,17 +1,41 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import UsersRoute from './routes/usersRoutes';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+export const prisma = new PrismaClient();
+
+
 
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Server is running BONJOUddddegiruytgughdfkugdhfikuR');
-});
+async function main() {
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    // Enregistrer les routes API
+    app.use('/users', UsersRoute);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log('coucoudfff');
-});
+    app.get('/', (req, res) => {
+        res.send('[USERS] Server is running !');
+    });
+
+    // // Capturer les routes non enregistrés
+    // app.all('*', (req, res) => {
+    //     res.status(404).json({ error: `Route ${req.originalUrl} not found` });
+    // });
+
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
+}
+
+main()
+    .then(async () => {
+        await prisma.$connect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
---
+++ backend/user-service/src/routes/usersRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import usersController from '../controllers/usersController';
+
+const UsersRoute:Router = Router();
+
+UsersRoute.post('/register', usersController.userRegister);
+
+
+export default UsersRoute;
